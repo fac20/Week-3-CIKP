643a12188063f8782b2a39fd5d8fd0c5b7ac33f0
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-07-29 16:39:53 +0100

    Creates display for task total and completed Relates #13

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const sun = document.querySelector('#sunCount');
+
+
+let sun = document.querySelector('#sunCount');
+let checkCount = document.querySelector('#checkedCount');
+let totalCount = document.querySelector('#totalCount');
+
+function showProgress(){
+    let checkNum=0;
+    let totalNum=0;
+    
+    checkCount.textContent = checkNum;
+    totalCount.textContent = totalNum;
+}
+
+showProgress();
